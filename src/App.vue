8d33d520309bb1c66f38b24b8399af0657cd1221
Tümer Bektaş Gediz <142098381+tumerbektas@users.noreply.github.com>
2024-08-02 11:44:28 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="container">
-    <div class="centered">
-      <p class="classic-font">EMRE TUNA</p>
-      <p class="subtitle">Agricultural Engineer</p>
-      <p class="description">
-        My name is Emre, and I am currently a senior student pursuing a degree in Agricultural Engineering at Ege University. I have completed my compulsory internship in the R&D department of Abalioglu Feed Company. From a young age, I have been deeply interested in and curious about animals. I am committed to continuously developing myself professionally in this field.
-      </p>
-      <div class="social-links">
-        <div class="links">
-          <div class="link-group">
-            <h3>CONNECT</h3>
-            <a href="mailto:etuna2867@gmail.com" target="_blank">Mail</a>
-            <a href="https://www.linkedin.com/in/emre-tuna-48316b289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">LinkedIn</a>
-          </div>
-          <div class="link-group">
-            <h3>SOCIAL</h3>
-            <a href="https://www.instagram.com/_emretuna_/" target="_blank">Instagram</a>
-            <a href="https://x.com/emretunna" target="_blank">X</a>
-          </div>
+  <div class="centered">
+    <p class="classic-font">EMRE TUNA</p>
+    <p class="subtitle">Agricultural Engineer</p>
+    <p class="description">
+      My name is Emre, and I am currently a senior student pursuing a degree in Agricultural Engineering at Ege University. I have completed my compulsory internship in the R&D department of Abalioglu Feed Company. From a young age, I have been deeply interested in and curious about animals. I am committed to continuously developing myself professionally in this field.
+    </p>
+    <div class="social-links">
+      <div class="links">
+        <div class="link-group">
+          <h3>CONNECT</h3>
+          <a href="mailto:etuna2867@gmail.com" target="_blank">Mail</a>
+          <a href="https://www.linkedin.com/in/emre-tuna-48316b289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">LinkedIn</a>
+        </div>
+        <div class="link-group">
+          <h3>SOCIAL</h3>
+          <a href="https://www.instagram.com/_emretuna_/" target="_blank">Instagram</a>
+          <a href="https://x.com/emretunna" target="_blank">X</a>
         </div>
       </div>
     </div>
@@ -32,37 +30,87 @@
 html, body {
   margin: 0;
   padding: 0;
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   background-color: #909090;
   color: #000000;
   font-family: 'Arial', sans-serif;
-}
-
-.container {
   display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  text-align: center;
 }
 
 .centered {
+  transform: translateY(-5%);
+}
+
+.classic-font {
+  font-size: 6rem;
+  font-family: 'Forta';
+  color: #000000;
+  margin: 0;
+}
+
+.subtitle {
+  font-size: 2rem;
+  color: #000000;
+  font-family: 'BGrove';
+  margin-top: 0.5rem;
+}
+
+.description {
+  font-size: 1.5rem;
+  color: #000000;
+  margin-top: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  font-family: 'BGrove';
+  max-width: 1000px;
+  line-height: 1.5;
+}
+
+.social-links {
+  margin-top: 3rem;
+}
+
+.links {
+  display: flex;
+  justify-content: center;
+  gap: 6rem;
+}
+
+.link-group {
   text-align: center;
-  padding-bottom: 4rem; /* Ensure space for the footer */
+  font-family: 'Rawen';
+}
+
+.link-group h3 {
+  font-size: 1.5rem;
+  color: #000000;
+  margin-bottom: 1.5rem;
+}
+
+.link-group a {
+  display: block;
+  color: #000000;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+}
+
+.link-group a:hover {
+  color: #ffffff;
 }
 
 .footer {
-  position: absolute;
+  position: fixed;
   bottom: 0;
   left: 0;
-  width: 100%;
   font-family: 'BGrove';
   font-size: 1rem;
   color: #000000;
-  text-align: center;
   padding: 0.5rem;
-  background-color: #ffffff; /* Add a background color if needed */
+  margin: 0;
 }
 
 /* Responsive Design */
